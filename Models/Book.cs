56cc2974577f94f7c8327a33e1a48d710307f72f
Jakub Laszczyk <jakub0907@gmail.com>
2022-03-04 21:45:25 +0100

    Task 2 - add bookform validation

--- Models/Book.cs
+++ Models/Book.cs
@@ -9,17 +9,23 @@ namespace LibApp.Models
     public class Book
     {
         public int Id { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Id is required!")]
 		[StringLength(255)]
 		public string Name { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Name is required!")]
 		public string AuthorName { get; set; }
-		[Required]
+		[Required(ErrorMessage = "AuthorName is required!")]
 		public Genre Genre { get; set; }
+		[Required(ErrorMessage = "Genre is required!")]
 		public byte GenreId { get; set; }
+		[Required(ErrorMessage = "GenreId is required!")]
 		public DateTime DateAdded { get; set; }
+		[Required(ErrorMessage = "DateAdded is required!")]
 		public DateTime ReleaseDate { get; set; }
+		[Required(ErrorMessage = "ReleaseDate is required!")]
 		public int NumberInStock { get; set; }
+		[Range(1, 20, ErrorMessage = "Number of stock must be bettwen {0} and {1}")]
+		[Required(ErrorMessage = "NumberInStock is required!")]
 		public int NumberAvailable { get; set; }
 	}
       
